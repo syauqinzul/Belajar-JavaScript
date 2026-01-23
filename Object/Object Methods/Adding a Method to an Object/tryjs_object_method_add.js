dff17ed54553a41f2502bd600ce64719ef5dca4a
syauqinzul <nuzulabdisyauqi@gmail.com>
2026-01-23 20:12:13 +0800

    object

---
+++ Object/Object Methods/Adding a Method to an Object/tryjs_object_method_add.js
@@ -0,0 +1,15 @@
+// Create an Object
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+};
+
+// Add a Method
+person.name = function() {
+  return this.firstName + " " + this.lastName;
+};
+
+// Display Object Data
+document.getElementById("demo").innerHTML =
+"My father is " + person.name(); 
