a39b2120bd1cb2855deef6d4bf5e36b54a0545ee
syauqinzul <nuzulabdisyauqi@gmail.com>
2026-01-23 20:00:03 +0800

    object

---
+++ Object/Object Properties/Nested Objects/tryjs_object_nested3.js
@@ -0,0 +1,11 @@
+// Create nested Objects
+const myObj = {
+  name: "John",
+  age: 30,
+  myCars: {
+    car1: "Ford",
+    car2: "BMW",
+    car3: "Fiat"
+  }
+}
+document.getElementById("demo").innerHTML = myObj["myCars"]["car2"];
