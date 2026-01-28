4c9837b2b1608aed7208cc411b5476036f727f14
syauqinzul <nuzulabdisyauqi@gmail.com>
2026-01-28 10:30:40 +0800

    DOM

---
+++ Dom/Finding HTML Elements by Tag Name/dom_getelementsbytagname.js
@@ -0,0 +1,5 @@
+const x = document.getElementById("intro");
+const y = x.document.getElementTagName("p");
+
+document.getElementById("demo").innerHTML =
+'paragraph pertama adalah (index ke-0) "intro"' + y[0].innerHTML;
---
+++ Dom/Finding HTML Elements by Tag Name/dom_getelementsbytagname2.js
@@ -0,0 +1,5 @@
+const elements = document.getElementsByTagName("p");
+
+document.getElementById("demo").innerHTML = 
+"The text from the first paragraph is: " + elements[0].innerHTML;
+
