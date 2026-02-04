d9b7d16836bf78fb208fd52d3a8c3827f5cacdd2
syauqinzul <nuzulabdisyauqi@gmail.com>
2026-02-04 22:09:55 +0800

    DOM

---
+++ Dom/JavaScript Form Validation/validations.js
@@ -0,0 +1,10 @@
+function validateForm() { 
+  let x = document.forms["myForm"]["fname"].value; 
+  if (x == "") { 
+    alert("Name must be filled out"); 
+    return false; 
+  } 
+}
+
+// --- IGNORE ---
+
